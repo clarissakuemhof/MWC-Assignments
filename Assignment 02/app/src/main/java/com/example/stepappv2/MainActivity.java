dfdabe92f485c5f5f5e870a48eb221e87f1e1844
Clarissa Kümhof <clarissakuemhof@AirvonClarissa.usilu.net>
2023-10-18 12:40:56 +0200

    added Assignment 02

---
+++ Assignment 02/app/src/main/java/com/example/stepappv2/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.stepappv2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.stepappv2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    private int counter = 0;
+    private TextView showCount;
+    private com.google.android.material.progressindicator.CircularProgressIndicator progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_steps, R.id.nav_report, R.id.nav_profile)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        showCount = findViewById(R.id.counter);
+
+        progressBar = findViewById(R.id.progressBar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void countUp(View view) {
+        counter ++;
+        showCount.setText(Integer.toString(counter));
+
+        String countText = showCount.getText().toString();
+        int number = Integer.valueOf(countText);
+
+        if (number >= 0 && number <= 100) {
+            progressBar.setProgress(number);
+        }
+    }
+
+    public void reset(View view) {
+        counter = 0;
+        showCount.setText(Integer.toString(counter));
+        progressBar.setProgress(0);
+    }
+}
---
+++ Assignment 02/app/src/main/java/com/example/stepappv2/ui/home/HomeFragment.java
@@ -0,0 +1,43 @@
+package com.example.stepappv2.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.stepappv2.R;
+import com.example.stepappv2.databinding.FragmentHomeBinding;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    private TextView stepCountsView;
+    private CircularProgressIndicator progressBar;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        stepCountsView = (TextView) root.findViewById(R.id.counter);
+
+        progressBar = (CircularProgressIndicator) root.findViewById(R.id.progressBar);
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
